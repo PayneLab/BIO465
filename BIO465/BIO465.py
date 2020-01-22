dec64c4d926ac16f157d0626c2d5c134df4a3118
Teancum Paquette <teancumskysmith@gmail.com>
2020-01-22 16:05:56 -0700

    download xlsx file and load into pandas dataframe

--- BIO465/BIO465.py
+++ BIO465/BIO465.py
@@ -1,4 +1,5 @@
-import pandas
+import pandas as pd
+import requests
 
 
 class BIO465:
@@ -11,11 +12,10 @@ class BIO465:
         self.dataframes = []
         self.homeworks = []
         self.labs = []
-        self.lab_links = {
-            0: "homework1.com"
-        }
+        self.lab_links = ["https://byu.box.com/shared/static/b0gn4i6v4h9a4owilv8of5m6x5tj82u7.xlsx"]
 
     """ retrieves a list of Lab objects from the instantiated class """
+
     def get_labs(self) -> list:
         return self.labs
 
@@ -32,6 +32,26 @@ class BIO465:
 
     def get_lab(self, lab_number: int) -> pandas.array:
         # query box to get the file
+        lab_link = self.lab_links[lab_number]
+        response = requests.get(lab_link, allow_redirects=True, stream=True)
+        xl = "lab1.xlsx"
+
+        with open(xl, "wb") as xl_file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    xl_file.write(chunk)
+
+        df = pd.read_excel(open('tmp.xlsx', 'rb'))
+        print(df)
+
+        with requests.Session() as session:  # Use a session object to save cookies
+
+            # Send initial GET request and parse the request token out of the response
+            get_response = session.get(lab_link)
+            #soup = bs4.BeautifulSoup(get_response.text, "html.parser")
+            #token_tag = soup.find(id="request_token")
+            #token = token_tag.get("value")
+        response.raise_for_status()  # Raises a requests.HTTPError if the response code was unsuccessful
         # read the file into file object
         # use pandas to convert to data frame
         # return data frame
