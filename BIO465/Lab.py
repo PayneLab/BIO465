eaf372affd43da6183b61f2675fa0804f7394f2e
thomashmolina <thomas.h.molina@gmail.com>
2020-01-27 17:30:16 -0700

    updated version

--- BIO465/Lab.py
+++ BIO465/Lab.py
@@ -1,4 +1,4 @@
-from BIO465 import Assignment
+from BIO465.Assignment import Assignment
 import pandas
 
 
@@ -11,17 +11,21 @@ class Lab(Assignment):
         self.dataframes = []
 
     """gets the dataframe file and returns a string of its contents"""
+
     def get_dataframe_file(self, file_path: str) -> str:  # TODO get the dataframe file path
         pass
 
     """parses the dataframe file and puts it into a pandas dataframe"""
+
     def parse_dataframe_file(self, file_string: str) -> pandas.array:  # TODO parse the dataframe string contents
         pass
 
     """To string method for Homework object"""
+
     def __str__(self) -> str:
         return f"Type: Lab\n{super.__str__()}"
 
     """How Lab will represent itself in the python console"""
+
     def __repr__(self) -> str:
         return self.__str__()
