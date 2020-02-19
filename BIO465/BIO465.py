1e69f6e596c0f3856e25da347a0edb2816d23fbd
Teancum Paquette <teancumskysmith@gmail.com>
2020-02-19 16:42:18 -0700

    Create multi-Index

--- BIO465/BIO465.py
+++ BIO465/BIO465.py
@@ -13,23 +13,39 @@ class BIO465:
         self.dataframes = []
         self.homeworks = []
         self.labs = []
-        self.lab_links = {'bacterial growth': "https://byu.box.com/shared/static/b0gn4i6v4h9a4owilv8of5m6x5tj82u7.xlsx"}
+        self.lab_links = {'Bacterial Growth': "https://byu.box.com/shared/static/b0gn4i6v4h9a4owilv8of5m6x5tj82u7.xlsx",
+                          'RNA Seq': "https://byu.box.com/shared/static/k5mdefkiqkj4bm9umd8naiaq20p1tpkk.txt",
+                          'Cancer Types': "https://byu.box.com/shared/static/u3czp6p3q76x2nh0x7rmiw8il2751ehk.txt"}
 
     """Queries box to get whatever link is within the lab_links parameter"""
+    def get_data_frame(self, lab_string: str, file_type: str):
+        file = f'file{file_type}'
+
+        lab_link = self.lab_links[lab_string]
+        response = requests.get(lab_link, allow_redirects=True, stream=True)  # query box to get the file
+        with open(file, "wb") as xl_file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    xl_file.write(chunk)
+
+        df = pd.read_excel(open(file, 'rb'))
+        os.remove('./' + file)
+
+        return df
 
     def get_lab(self, lab_string: str) -> pd.array:
         """
         :type lab_string: str
         """
-        ErrorMessage = "Lab does not exist, returning empty pandas array."
+        error_message = "Lab does not exist, returning empty pandas array."
         if not isinstance(lab_string, str):
-            print(ErrorMessage)
+            print(error_message)
             return pd.array([])
         lab_string = lab_string.lower()
         if lab_string not in self.lab_links.keys():
-            print(ErrorMessage)
+            print(error_message)
             return pd.array([])
-
+        """
         lab_link = self.lab_links[lab_string]
         response = requests.get(lab_link, allow_redirects=True, stream=True)  # query box to get the file
         xl = "lab.xlsx"
@@ -40,7 +56,10 @@ class BIO465:
 
         df = pd.read_excel(open(xl, 'rb'))
         os.remove('./' + xl)
-        if lab_string == 'bacterial growth':
+        """
+        if lab_string == 'Bacterial Growth':
+            file_type = ".xlsx"
+            df = self.get_data_frame(lab_string, file_type)
             df = df.drop(columns=["Min_MSGF", "Peptides", "Ref1", "Ref2", "Ref3", "Ref4", "Spectra"])
             df = df.set_index(["Protein"])
             df = df.transpose()
@@ -49,12 +68,17 @@ class BIO465:
             plate_type = temp_cols[0]
             time_plate = temp_cols[1].str.split('.', n=1, expand=True)
             df = df.assign(oxidation=plate_type, time=time_plate[0], plate=time_plate[1])
-            df = df.set_index(["experimental condition", "time point", "replicate"])
-            df = df.sort_index()
-            df = df.transpose()
+            df = df.set_index(["Experimental Condition", "Time Point", "Replicate"])
+            df = df.sort_index().transpose()
+        if lab_string == 'RNA Seq':
+            file_type = ".txt"
+            df = self.get_data_frame(lab_string, file_type)
+        if lab_string == 'Cancer Types':
+            file_type = ".txt"
+            df = self.get_data_frame(lab_string, file_type)
         return df
 
 
 if __name__ == "__main__":
     b = BIO465()
-    df_oxidation = [col.split('_')[0] for col in df if col.startswith("O2") or col.startswith("An")]
+    # df_oxidation = [col.split('_')[0] for col in df if col.startswith("O2") or col.startswith("An")]
